587da5eb2e61bb13ac757ca0a43249e99bab3dc5
Matteo Moci <mox601@libero.it>
2014-07-16 13:45:33 +0200

    added script to run server

---
+++ twofishes-client/scripts/server.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# java -Xms1g -Xmx1g -jar server-assembly-0.82.14.jar --hfile_basepath latest/ --port 5100 --run_http_server true
+
+#some variables make it more flexible
+SERVER_ASSEMBLY="server-assembly-0.82.14.jar"
+HFILE_BASEPATH="latest/"
+PORT="5100"
+RUN_HTTP_SERVER="true"
+
+java -Xms1g -Xmx1g -jar $SERVER_ASSEMBLY --hfile_basepath $HFILE_BASEPATH --port $PORT --run_http_server $RUN_HTTP_SERVER
